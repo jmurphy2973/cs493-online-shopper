5910983d65742874c084bd6d8a97166592548bba
John Murphy <John@TrueAnalyticsSolutions.com>
2022-12-04 14:28:53 -0500

    Added requirements file
    Resolved issue on google app engine with building container missing python modules.

--- main.py
+++ main.py
@@ -933,4 +933,4 @@ def assign_remove_product_to_shopper(shopper_id, product_id):
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
